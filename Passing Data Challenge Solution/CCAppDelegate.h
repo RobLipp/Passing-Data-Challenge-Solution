8dfed199be36f3dd9b040690ae957e7ef0afc9c7
Rob Lipp <lunarwild@gmail.com>
2015-09-18 07:18:04 -0700

    Initial Commit

---
+++ Passing Data Challenge Solution/CCAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  CCAppDelegate.h
+//  Passing Data Challenge Solution
+//
+//  Created by Rob Lipp on 2015-09-18.
+//  Copyright (c) 2015 Rob Lipp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CCAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
